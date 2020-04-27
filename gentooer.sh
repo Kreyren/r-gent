5f3186877660cefdd1ecb72822a76800ab9bc2ac
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-04-27 12:58:44 +0600

    Update gentooer.sh

--- gentooer.sh
+++ gentooer.sh
@@ -2,7 +2,7 @@
 
 mount_dir="/mnt/gentoo" 
 mount /dev/sdb2 "$mount_dir"
-cd "$mount_dir"
+cd "$mount_dir" || exit
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
