bc10eededb384f2d80039c30cf58d84568e371b0
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-04-27 12:59:42 +0600

    Update getgentoo.sh

--- getgentoo.sh
+++ getgentoo.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 mount_dir="/mnt/gentoo" 
-stage3_link = https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20200422T214502Z/stage3-amd64-20200422T214502Z.tar.xz
+stage3_link="https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20200422T214502Z/stage3-amd64-20200422T214502Z.tar.xz"
 
 mkdir "$mount_dir"
 mount /dev/sdb2 "$mount_dir"
-cd "$mount_dir"
+cd "$mount_dir" || exit
 wget "$stage3_link"
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 
-cd "$mount_dir"/etc/portage/
+cd "$mount_dir"/etc/portage/ || exit
 rm make.conf
 wget -O make.conf https://raw.githubusercontent.com/Uniminin/portage-config/master/make.conf2
-cd ; mkdir --parents "$mount_dir"/etc/portage/repos.conf
+cd || exit ; mkdir --parents "$mount_dir"/etc/portage/repos.conf
 cp "$mount_dir"/usr/share/portage/config/repos.conf "$mount_dir"/etc/portage/repos.conf/gentoo.conf
 cp --dereference /etc/resolv.conf "$mount_dir"/etc/
 
